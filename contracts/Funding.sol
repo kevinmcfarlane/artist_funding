3043eb71f17d655bd61cfd03fc5c2055de153f7e
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2019-02-04 14:11:30 +0000

    Add unit test for setting an owner.

--- contracts/Funding.sol
+++ contracts/Funding.sol
@@ -1,4 +1,12 @@
 pragma solidity >=0.4.21 <0.6.0;
 
+/// @author Kevin McFarlane
+/// @title Represents funding of an artist.
 contract Funding {
+    address public owner;
+
+    constructor() public {
+        // A sender of the message inside the constructor is a deployer.
+        owner = msg.sender;
+    }
 }
--- test/FundingTest.sol
+++ test/FundingTest.sol
@@ -4,4 +4,8 @@ import "truffle/Assert.sol";
 import "../contracts/Funding.sol";
 
 contract FundingTest {
+    function testSettingAnOwnerDuringCreation() public {
+        Funding funding = new Funding();
+        Assert.equal(funding.owner(), this, "Owner should be the same as deployer.");
+    }
 }

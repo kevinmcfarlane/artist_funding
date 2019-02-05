d4aed78716d28a7c191f546ace8c7fdeacee6397
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2019-02-05 14:26:32 +0000

    Add test for setting owner in migrated contract.

--- test/FundingTest.sol
+++ test/FundingTest.sol
@@ -1,11 +1,17 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
 import "../contracts/Funding.sol";
 
 contract FundingTest {
     function testSettingAnOwnerDuringCreation() public {
         Funding funding = new Funding();
         Assert.equal(funding.owner(), this, "Owner should be the same as deployer.");
+    }
+
+    function testSettingAnOwnerOfDeployedContract() public {
+        Funding funding = Funding(DeployedAddresses.Funding());
+        Assert.equal(funding.owner(), msg.sender, "Owner should be the same as deployer.");
     }
 }

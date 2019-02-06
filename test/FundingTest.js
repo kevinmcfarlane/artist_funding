9411b316f54d0b8fd4cb96bfa29df198f84e6428
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2019-02-06 16:50:01 +0000

    Add donate functionality. Add tests to verify basic donation.

--- test/FundingTest.js
+++ test/FundingTest.js
@@ -1,5 +1,7 @@
 const Funding = artifacts.require("Funding");
 
+const FINNEY = 10**15;
+
 // contract (instead of describe) does some cleanup and provides a list of available accounts. 
 // The first account is used by default during tests.
 contract("Funding", accounts => {
@@ -8,5 +10,11 @@ contract("Funding", accounts => {
   it("sets an owner", async () => {
     const funding = await Funding.new();
     assert.equal(await funding.owner.call(), firstAccount);
+  });
+
+  it("accepts donations", async () => {
+    const funding = await Funding.new();
+    await funding.donate({ from: firstAccount, value: 10 * FINNEY });
+    assert.equal(await funding.raised.call(), 10 * FINNEY);
   });
 });
--- test/FundingTest.sol
+++ test/FundingTest.sol
@@ -5,6 +5,11 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/Funding.sol";
 
 contract FundingTest {
+    // Truffle will send the TestContract Ether after deploying the contract.
+    // See https://truffleframework.com/docs/truffle/testing/writing-tests-in-solidity#in-beta-testing-ether-transactions
+    // We need this to test donation.
+    uint public initialBalance = 10 ether;
+    
     function testSettingAnOwnerDuringCreation() public {
         Funding funding = new Funding();
         Assert.equal(funding.owner(), this, "Owner should be the same as deployer.");
@@ -13,5 +18,12 @@ contract FundingTest {
     function testSettingAnOwnerOfDeployedContract() public {
         Funding funding = Funding(DeployedAddresses.Funding());
         Assert.equal(funding.owner(), msg.sender, "Owner should be the same as deployer.");
+    }
+
+    function testAcceptingDonations() public {
+        Funding funding = new Funding();
+        Assert.equal(funding.raised(), 0, "Raised amount should be 0.");
+        funding.donate.value(10 finney)();
+        Assert.equal(funding.raised(), 10 finney, "Raised amount should equal donation.");
     }
 }
